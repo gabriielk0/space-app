0d90d2b09d960f1364dc2d79406902e36061a1c4
Gabriel <oliveiracgabriel052@gmail.com>
2024-10-17 19:50:03 -0300

    Adicionando Sessão TAGS

---
+++ src/componentes/Galeria/Tags/index.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+import tags from './tags.json'
+
+const SecaoTags = styled.section`
+    display: flex;
+    gap: 24px;
+    margin-top: 20px;
+    align-items: center;
+`
+
+const TextoEstilizado = styled.p`
+    color: #FFFFFF;
+`
+
+const Botao = styled.button`
+    color: #FFFFFF;
+    background-color: #D9D9D950;
+    width: auto;
+    height: 39px;
+    border: solid 2px #7B78E5;
+    border-radius: 10px;
+`
+
+
+const Tags = () => {
+    return (
+        <SecaoTags>
+            <TextoEstilizado>Busque por tags:</TextoEstilizado>
+            {tags.map(tag => <Botao key={tag.id}>{tag.titulo}</Botao>)}
+        
+        </SecaoTags>
+    )
+
+}
+
+export default Tags
